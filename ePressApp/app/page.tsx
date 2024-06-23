2f6439bd799dfd346c76bd66a7662de2c82e594a
Julio M Cruz <julio.cruz@eb-ms.net>
2024-06-23 12:06:38 -0500

    update layout

---
+++ ePressApp/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import HeaderComponent from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+}
---
+++ ePressApp/constants/siteInfo.js
@@ -0,0 +1,25 @@
+// Description: Site information constants.
+export const SITE_NAME = "ePress";
+export const SITE_LOGO = "/resources/logo-placeholder-clear.png";
+export const MENU_ITEMS = [
+    { 
+        label: "Home", 
+        href: "/",
+        icon: "/resources/icons8-home.svg" 
+    },
+    { 
+        label: "About", 
+        href: "/about",
+        icon: "/resources/icons8-about.svg"
+    },
+    { 
+        label: "Services", 
+        href: "/services",
+        icon: "/resources/icons8-services.svg"
+    },
+    { 
+        label: "Contact", 
+        href: "/contact",
+        icon: "/resources/icons8-contact-us-50.png"
+    },
+];
