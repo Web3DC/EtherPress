05efb86cd58e6f1146caaf57334b44329036451e
Julio M Cruz <julio.cruz@eb-ms.net>
2024-06-25 14:49:12 -0400

    Publishing Resources Component

---
+++ ePressApp/app/editor-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import EditorBlogListComponent from "@/components/EditorBlogList";
+import HeaderComponent from "@/components/Header";
+
+export default function EditorDashboard() {
+    return (
+      <div>
+        <HeaderComponent />
+        <EditorBlogListComponent />
+      </div>
+    );
+  }
