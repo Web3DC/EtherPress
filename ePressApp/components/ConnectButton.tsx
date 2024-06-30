c21fcfb63f96a1b93695ee4cfc2ae15ae4b751da
Diego Bejar <diebejardelaguila@gmail.com>
2024-06-30 07:02:12 -0500

    Add validate exist wallet

--- ePressApp/components/ConnectButton.tsx
+++ ePressApp/components/ConnectButton.tsx
@@ -1,13 +1,11 @@
 "use client";
 
+import React, { useEffect } from 'react';
+import { createWallet } from "thirdweb/wallets";
+import { ConnectButton } from "thirdweb/react";
+import { useAppContext } from "@/context/GlobalContext";
 import { thirdwebClient } from "@/utils/thirdweb";
 import { baseSepolia, defineChain } from "thirdweb/chains";
-
-import { createWallet, } from "thirdweb/wallets";
-import { ConnectButton, } from "thirdweb/react";
-
-import { useContext } from 'react';
-import { useAppContext } from "@/context/GlobalContext";
 
 const walletsSetup = [
   createWallet("com.coinbase.wallet", {
@@ -18,98 +16,80 @@ const walletsSetup = [
 ];
 
 interface TitleComponentProps {
-    labelButton: string;
-  }
+  labelButton: string;
+}
 
 const ConnectButtonComponent: React.FC<TitleComponentProps> = ({ labelButton }) => {
-
-  const {userIsMember, setUserIsMember} = useAppContext();
-  const {userIsWriter, setUserIsWriter} = useAppContext();
-  const {userIsAdmin, setUserIsAdmin} = useAppContext();
+  const { setUserIsMember, setUserIsWriter, setUserIsAdmin } = useAppContext();
 
   const handleConnect = (userInfo: any) => {
-    // Handle the disconnect event here
     const account = userInfo.getAccount();
-    //
-    // Check if user is Admin, Writer or Member
-    const memberWhiteList = process.env.NEXT_PUBLIC_MEMBER_ADDRESS_WHITE_LIST || "";
-    if (memberWhiteList.length > 0) {
-      const memberList = memberWhiteList.split(",");
-      if (account && account.address) {
-        if (memberList.includes(account.address)) {
-          console.log("User is Member: ", account.address);
+
+    if (account && account.address) {
+      const address = account.address.toLowerCase(); // Normalizar la dirección a minúsculas
+
+      // Verificar si el usuario está en la lista blanca de miembros
+      const memberWhiteList = process.env.NEXT_PUBLIC_MEMBER_ADDRESS_WHITE_LIST || "";
+      if (memberWhiteList.length > 0) {
+        const memberList = memberWhiteList.split(",").map(addr => addr.trim().toLowerCase());
+        if (memberList.includes(address)) {
+          console.log("User is Member: ", address);
           setUserIsMember(true);
-        }  
+        } else {
+          setUserIsMember(false);
+        }
       }
-    }
 
-    const writerWhiteList = process.env.NEXT_PUBLIC_WRITER_ADDRESS_WHITE_LIST || "";
-    if (writerWhiteList.length > 0) {
-      const writerList = writerWhiteList.split(",");
-      if (account && account.address) {
-        if (writerList.includes(account.address)) {
-          console.log("User is Writer: ", account.address);
+      // Verificar si el usuario está en la lista blanca de escritores
+      const writerWhiteList = process.env.NEXT_PUBLIC_WRITER_ADDRESS_WHITE_LIST || "";
+      if (writerWhiteList.length > 0) {
+        const writerList = writerWhiteList.split(",").map(addr => addr.trim().toLowerCase());
+        if (writerList.includes(address)) {
+          console.log("User is Writer: ", address);
           setUserIsWriter(true);
-        }  
+        } else {
+          setUserIsWriter(false);
+        }
       }
-    }
 
-    const adminWhiteList = process.env.NEXT_PUBLIC_ADMIN_ADDRESS_WHITE_LIST || "";
-    if (adminWhiteList.length > 0) {
-      const adminList = adminWhiteList.split(",");
-      if (account && account.address) {
-        if (adminList.includes(account.address)) {
-          console.log("User is Admin: ", account.address);
+      // Verificar si el usuario está en la lista blanca de administradores
+      const adminWhiteList = process.env.NEXT_PUBLIC_ADMIN_ADDRESS_WHITE_LIST || "";
+      if (adminWhiteList.length > 0) {
+        const adminList = adminWhiteList.split(",").map(addr => addr.trim().toLowerCase());
+        if (adminList.includes(address)) {
+          console.log("User is Admin: ", address);
           setUserIsAdmin(true);
+        } else {
+          setUserIsAdmin(false);
         }
       }
     }
-
-    // wait 1 second for the state to be updated
-    setTimeout(() => {
-      console.log("User is Admin: ", userIsAdmin);
-      console.log("User is Writer: ", userIsWriter);
-      console.log("User is Member: ", userIsMember);
-    }, 1000);
-
-    //
-    // console.log("User is Admin: ", userIsAdmin);
-    // console.log("User is Writer: ", userIsWriter);
-    // console.log("User is Member: ", userIsMember);
-    //
-
-
-  };  
+  };
 
   const handleDisconnect = () => {
-    // Handle the disconnect event here
+    // Manejar el evento de desconexión aquí
     console.log("Wallet disconnected");
     setUserIsMember(false);
     setUserIsWriter(false);
     setUserIsAdmin(false);
-    //
-    console.log("User is Admin: ", userIsAdmin);
-    console.log("User is Writer: ", userIsWriter);
-    console.log("User is Member: ", userIsMember);
-    //
-  };  
-    
-    return (
-        <ConnectButton
-                    client={thirdwebClient}
-                    wallets={walletsSetup}
-                    theme={"light"}
-                    connectButton={{ label: labelButton || "Connect" }}
-                    connectModal={{
-                      size: "compact",
-                      title: "Instant Cash",
-                      showThirdwebBranding: false,
-                    }}
-                    chain={defineChain(baseSepolia)}
-                    onConnect={handleConnect}
-                    onDisconnect={handleDisconnect}
-                  />
-    )
+  };
+
+  return (
+    <ConnectButton
+      client={thirdwebClient}
+      wallets={walletsSetup}
+      theme={"light"}
+      connectButton={{ label: labelButton || "Connect" }}
+      connectModal={{
+        size: "compact",
+        title: "Instant Cash",
+        showThirdwebBranding: false,
+      }}
+      chain={defineChain(baseSepolia)}
+      onConnect={handleConnect}
+      onDisconnect={handleDisconnect}
+    />
+  );
 };
 
 export default ConnectButtonComponent;
